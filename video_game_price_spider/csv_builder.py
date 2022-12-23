aea57f48a3e6d93dc496ea16f19825ea96195b3d
tlegasse-miir <tanner.legasse@gmail.com>
2022-12-22 17:51:37 -0800

    Removing unused var

--- video_game_price_spider/csv_builder.py
+++ video_game_price_spider/csv_builder.py
@@ -2,25 +2,12 @@ import os
 from typing import List, Dict
 import pandas as pd
 
+
 class CsvBuilder:
     _base_path = "./price_entries"
     _full_path: str
     _console: str
     _product_data: List[Dict] = []
-    _columns_to_write: List[str] = [
-        "consoleUri",
-        "hasProduct",
-        "id",
-        "price1",
-        "price2",
-        "price3",
-        "priceChange",
-        "priceChangePercentage",
-        "priceChangeSign",
-        "printRun",
-        "productName",
-        "productUri"
-    ]
 
     def __init__(self, product_data: List[Dict], console: str):
         self.set_product_data(product_data)
@@ -30,7 +17,6 @@ class CsvBuilder:
         if not os.path.isdir(self._base_path):
             os.makedirs(self._base_path)
 
-
     def set_product_data(self, product_data: List[Dict]):
         self._product_data = product_data
 
@@ -39,12 +25,11 @@ class CsvBuilder:
 
     def get_full_path(self):
         return self._full_path
-        
+
     def build_full_path(self):
         self._full_path = f"{self._base_path}/{self._console}.csv"
 
     def write_product_data_to_csv(self):
-
         prepared_data = {}
 
         for item in self._product_data:
@@ -53,9 +38,6 @@ class CsvBuilder:
                     prepared_data[column] = []
 
                 prepared_data[column].append(item[column])
-                
 
-        df = pd.DataFrame(data=prepared_data)
-        df.to_csv(self._full_path)
-
-
+        data_frame = pd.DataFrame(data=prepared_data)
+        data_frame.to_csv(self._full_path)
