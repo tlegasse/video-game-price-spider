4dedc7f224c5d706f91b4042577d68a6a6556cdb
tlegasse-miir <tanner.legasse@gmail.com>
2022-12-23 18:12:03 -0800

    Typing out some classes

--- video_game_price_spider/csv_builder.py
+++ video_game_price_spider/csv_builder.py
@@ -1,15 +1,14 @@
 import os
-from typing import List, Dict
 import pandas as pd
 
 
 class CsvBuilder:
-    _base_path = "./price_entries"
+    _base_path: str = "./price_entries"
     _full_path: str
     _console: str
-    _product_data: List[Dict] = []
+    _product_data: list = []
 
-    def __init__(self, product_data: List[Dict], console: str):
+    def __init__(self, product_data: list, console: str)-> None:
         self.set_product_data(product_data)
         self.set_console(console)
         self.build_full_path()
@@ -17,20 +16,20 @@ class CsvBuilder:
         if not os.path.isdir(self._base_path):
             os.makedirs(self._base_path)
 
-    def set_product_data(self, product_data: List[Dict]):
-        self._product_data = product_data
+    def set_product_data(self, product_data: list)-> None:
+        self._product_data: list = product_data
 
-    def set_console(self, console):
-        self._console = console
+    def set_console(self, console)-> None:
+        self._console: str = console
 
-    def get_full_path(self):
+    def get_full_path(self)-> str:
         return self._full_path
 
-    def build_full_path(self):
-        self._full_path = f"{self._base_path}/{self._console}.csv"
+    def build_full_path(self)-> None:
+        self._full_path: str = f"{self._base_path}/{self._console}.csv"
 
-    def write_product_data_to_csv(self):
-        prepared_data = {}
+    def write_product_data_to_csv(self)-> None:
+        prepared_data:dict = {}
 
         for item in self._product_data:
             for column in item.keys():
@@ -39,5 +38,5 @@ class CsvBuilder:
 
                 prepared_data[column].append(item[column])
 
-        data_frame = pd.DataFrame(data=prepared_data)
+        data_frame:pd.DataFrame = pd.DataFrame(data=prepared_data)
         data_frame.to_csv(self._full_path)
--- video_game_price_spider/product_data_query.py
+++ video_game_price_spider/product_data_query.py
@@ -1,47 +1,45 @@
 import requests
-from typing import List, Dict
-import time
 
 class ProductDataQuery:
-    _base_url = "https://www.pricecharting.com/console"
-    _cursor = 0
-    _return_format = "json"
-    _product_data = []
-    _console = ""
+    _base_url:str = "https://www.pricecharting.com/console"
+    _cursor: int = 0
+    _return_format:str = "json"
+    _product_data:list = []
+    _console:str = ""
 
-    def __init__(self):
-        self._endpoint = self.get_endpoint()
-        self._params = self.get_params()
+    def __init__(self) -> None :
+        self._endpoint:str = self.get_endpoint()
+        self._params:dict = self.get_params()
 
-    def set_console(self,console:str):
-        self._console = console
+    def set_console(self,console:str)-> None:
+        self._console: str = console
 
-    def set_cursor(self, cursor):
-        self._cursor = cursor
+    def set_cursor(self, cursor)-> None:
+        self._cursor: int = cursor
 
-    def get_console(self):
+    def get_console(self)->str :
         return self._console
 
-    def get_base_url(self):
+    def get_base_url(self)-> str:
         return self._base_url
 
-    def get_cursor(self):
+    def get_cursor(self)-> int:
         return self._cursor
 
-    def get_product_data(self):
+    def get_product_data(self)-> list:
         return self._product_data
 
-    def get_params(self):
+    def get_params(self) -> dict:
         return {
             "cursor": self._cursor,
             "format": self._return_format
         }
 
-    def append_product_data(self, products: List[Dict]):
+    def append_product_data(self, products: list)-> None:
         self._product_data.extend(products)
 
-    def call_data(self):
-        response = requests.request(
+    def call_data(self)-> None:
+        response: requests.Response = requests.request(
             "GET",
             self.get_endpoint(),
             params = self.get_params()
@@ -51,7 +49,7 @@ class ProductDataQuery:
             print(f"Couldn't query for {self.get_console()}")
             return
 
-        response_json = response.json()
+        response_json:dict = response.json()
 
         self.append_product_data(response_json['products'])
 
@@ -63,5 +61,5 @@ class ProductDataQuery:
         if self._cursor:
             self.call_data()
 
-    def get_endpoint(self):
+    def get_endpoint(self)-> str:
         return f"{self._base_url}/{self._console}"
